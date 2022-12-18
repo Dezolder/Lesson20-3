e6d520f0e4bb73003f0c8c760b27c2d093242d4d
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-12-19 00:43:11 +0200

    Lesson20-3 Learned

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
@@ -10,16 +10,15 @@ import { Provider } from "react-redux";
 import history from "./app/utils/history";
 
 const store = createStore();
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-    <Provider store={store}>
-        <Router history={history}>
-            <React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router history={history}>
                 <App />
-            </React.StrictMode>
-        </Router>
-    </Provider>
+            </Router>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
